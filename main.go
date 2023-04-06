b1158395573d8ce52a34116e8f37485642aa439c
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-04-06 14:29:02 +0200

    add slices (arrays with no fixed size)

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	conferenceName := "Go Conference"
-	const conferenceTickets int = 50
+	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T and remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
@@ -12,10 +12,12 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v and %v are still available\n", conferenceTickets, remainingTickets)
 
+	bookings := []string{}
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name")
 	fmt.Scan(&firstName)
@@ -29,5 +31,10 @@ func main() {
 	fmt.Println("How many tickets you need?")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("%v %v booked %v tickets using %v email address.\n", firstName, lastName, userTickets, email)
+	remainingTickets = conferenceTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("%v booked %v tickets using %v email address.\n", bookings[0], userTickets, email)
+	fmt.Printf("%v tickets are remained.\n", remainingTickets)
+	fmt.Printf("%v\n", len(bookings))
 }
