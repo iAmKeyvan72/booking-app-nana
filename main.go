947552652e7d1cfd16360c743c905d003489ed5a
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-04-06 17:37:00 +0200

    split funcs into multiple files

--- main.go
+++ main.go
@@ -52,14 +52,6 @@ func getFirstNames(bookings []string) []string {
 	return firstNames
 }
 
-func validateUserInputs(firstName string, lastName string, email string, userTickets int, remainingTickets int) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
