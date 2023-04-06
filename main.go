febe35e2f23dc6b683ee817f374ea447f6f175d0
Keyvan Mahmoudi <5063636+iAmKeyvan72@users.noreply.github.com>
2023-04-06 16:46:44 +0200

    add validation

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ func main() {
 	bookings := []string{}
 
 	for {
-
 		var firstName string
 		var lastName string
 		var email string
@@ -36,32 +35,36 @@ func main() {
 		fmt.Println("How many tickets you need?")
 		fmt.Scan(&userTickets)
 
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining\n", remainingTickets)
-			break
-		}
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		if isValidName && isValidEmail && isValidTicketNumber {
 
-		fmt.Printf("%v booked %v tickets using %v email address.\n", bookings[0], userTickets, email)
-		fmt.Printf("%v tickets are remained.\n", remainingTickets)
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
+			fmt.Printf("%v booked %v tickets using %v email address.\n", bookings[0], userTickets, email)
+			fmt.Printf("%v tickets are remained.\n", remainingTickets)
 
-		for _, booking := range bookings {
-			names := strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
+			firstNames := []string{}
 
-		fmt.Printf("First names of the bookings are: %v\n", firstNames)
-		fmt.Printf("the array has %v items\n", len(bookings))
+			for _, booking := range bookings {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
 
-		noTickets := remainingTickets == 0
+			fmt.Printf("First names of the bookings are: %v\n", firstNames)
+			fmt.Printf("the array has %v items\n", len(bookings))
 
-		if noTickets {
-			fmt.Println("Booked out!")
-			break
+			noTickets := remainingTickets == 0
+
+			if noTickets {
+				fmt.Println("Booked out!")
+				break
+			}
+		} else {
+			fmt.Printf("No a valid info provided\n")
 		}
 	}
 
